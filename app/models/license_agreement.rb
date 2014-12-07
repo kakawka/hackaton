bae018e41e149747a8ced1b8f22068ffc8d92c12
Denis Lukyanov <kakawka05@gmail.com>
2014-12-07 19:26:54 +0300

    fix main chart and add limits

--- app/models/license_agreement.rb
+++ app/models/license_agreement.rb
@@ -2,7 +2,7 @@ class LicenseAgreement < ActiveRecord::Base
   belongs_to :user
   has_many :license_agreement_terms
   has_many :license_agreement_acceptances
-  accepts_nested_attributes_for :license_agreement_terms, :reject_if => :all_blank
+  accepts_nested_attributes_for :license_agreement_terms, :reject_if => :all_blank, :allow_destroy => true
 
 
   validates :name, :domain, :user_id, presence: true
