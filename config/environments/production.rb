2ca9b03b728a94b2d1820df58674ce8224325619
Ivan <ivan@ip-larionov.ru>
2014-12-08 10:22:39 +0400

    assets host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.host = 'lvh.me:3000'
+  config.host = 'accepttheterms.ivanthecrazy.koding.io'
 end
